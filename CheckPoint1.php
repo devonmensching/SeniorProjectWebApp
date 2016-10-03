cc75517eb6235760350d69ac5259f3c5501e5fe4
devonmensching <devonmensching@gmail.com>
2016-10-02 22:44:51 -0400

    Fixed hard coded access tokens

--- CheckPoint1.php
+++ CheckPoint1.php
@@ -1,4 +1,6 @@
-
+<?php
+		include('connection.php');
+?>
 <html>
 <head>
 	<h1> Check Point #1 </h1>
@@ -13,19 +15,18 @@
 			<td><Strong>Password</Strong></td>
 		</tr>
 		<?php
-		include('connection.php');
-
-		$sql = "SELECT * FROM Users";
+		$sql = "SELECT Username, MD5(Password) FROM Users";
 		foreach($connection->query($sql) as $result)
 		{
 			echo "<tr>";
 			echo "<td>" . $result["Username"] . "</td>";
-			echo "<td>" . $result["Password"] . "</td>";
+			echo "<td>" . $result["MD5(Password)"] . "</td>";
 			echo "</tr>";
 		}
 		?>
 	</table>
-	<p>A query to Insert into the User Table: INSERT INTO Users VALUES("username","password")</p>
+	<p>A query to Insert into the User Table: INSERT INTO Users VALUES("BChelmowski", MD5("woiuyt"))</p>
+	<p>Results from Insert into the User Table Query: Row Inserted - Username: BChelmowski; password: bd71fc1a70c4209d522a08f1a87ef77a;
 	<br><br>
 	
 	
@@ -41,8 +42,6 @@
 			<td><Strong>DayEnd</Strong></td>
 		</tr>
 		<?php
-		include('connection.php');
-
 		$sql = "SELECT * FROM Participants";
 		foreach($connection->query($sql) as $result)
 		{
@@ -56,7 +55,8 @@
 		}
 		?>
 	</table>
-	<p>A query to Insert into the Participants Table: INSERT INTO Participants VALUES("username","password", "Name", "DayStart", "DayEnd")</p>
+	<p>A query to Insert into the Participants Table: INSERT INTO Participants VALUES("MJohnson", MD5("abcdef"), "Michael", "8:00 AM", "4:00 PM")</p>
+	<p>Results from Insert into Participation Table Query: Row Inserted - Username: MJohnson; Password: e80b5017098950fc58aad83c8c14978e; Name: Michael; DayStart: 8:00 AM; DayEnd: 4:00 PM </p>
 	<br><br>
 	
 	
@@ -70,8 +70,6 @@
 			<td><Strong>Zone</Strong></td>
 		</tr>
 		<?php
-		include('connection.php');
-
 		$sql = "SELECT * FROM ZoneData";
 		foreach($connection->query($sql) as $result)
 		{
@@ -83,7 +81,29 @@
 		}
 		?>
 	</table>
-	<p>A query to Search into the ZoneData Table: SELECT * FROM ZoneData WHERE _____ = _____</p>
+	<p>A query to Search into the ZoneData Table: SELECT * FROM ZoneData WHERE Username = "HAnderson"</p>
+	<p>Results from SELECT from ZoneData Table Query: <table>
+														<tr>
+															<td>Username</td>
+															<td>Time</td>
+															<td>Zone</td>
+														</tr>
+														<tr>
+															<td>HAnderson</td>
+															<td>8:00 AM</td>
+															<td>GREEN</td>
+														</tr>
+														<tr>
+															<td>HAnderson</td>
+															<td>9:00 AM</td>
+															<td>GREEN</td>
+														</tr>
+														<tr>
+															<td>HAnderson</td>
+															<td>10:00 AM</td>
+															<td>BLUE</td>
+														</tr>
+													  <table></p>
 	<br><br>
 	
 <!-- End ZoneData table Infomration --->
--- connection.php
+++ connection.php
@@ -1,21 +1,17 @@
 <?php
 
-$DB_SERVER = "localhost";
-$DB_NAME = "SeniorProject";
-$DB_USER = "root";
-$DB_PASS = "shiraz";
+require("constants.php");
 
 // 1. Create a database connection and select database
 
 try {
 
-	$connection = new PDO('mysql:host=' . $DB_SERVER . ';dbname=' . $DB_NAME, $DB_USER, $DB_PASS);	
+	$connection = new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_NAME, DB_USER, DB_PASS);	
 	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
 } catch (PDOException $e) {
 
 	echo "Could not connect.";
-	echo $e;
 	exit();
 
 }
